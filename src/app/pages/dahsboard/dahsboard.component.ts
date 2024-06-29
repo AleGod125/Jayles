303f09589382365193649c4f319e4823ba63dfbf
AleGod125 <alejandroviolori@gmail.com>
2024-06-29 11:10:28 -0500

    cambios

--- src/app/pages/dahsboard/dahsboard.component.ts
+++ src/app/pages/dahsboard/dahsboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,11 +6,19 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './dahsboard.component.html',
-  styleUrl: './dahsboard.component.css'
+  styleUrls: ['./dahsboard.component.css'] // Corrección de 'styleUrl' a 'styleUrls'
 })
-export class DahsboardComponent {
+export class DahsboardComponent implements AfterViewInit {
   Catalogo: string = "Catalogo";
 
+  ngAfterViewInit(): void {
+    const carouselElement = document.querySelector('#carouselExampleSlidesOnly');
+    if (carouselElement) {
+      new (window as any).bootstrap.Carousel(carouselElement, {
+        interval: 3000 
+      });
+    }
+  }
 
   images: Array<{url: string, alt: string}> = [
     { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FgaL0znZDCifO0nkD4oD469n5aN1.jpg&w=256&q=75', alt: 'Imagen 1' },
@@ -21,8 +29,6 @@ export class DahsboardComponent {
     { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FzubzDKFoOH4VpfHWvLfhDss6fua.jpg&w=256&q=75', alt: 'Imagen 3' },
     { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FgaL0znZDCifO0nkD4oD469n5aN1.jpg&w=256&q=75', alt: 'Imagen 1' },
     { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2Fx7phbLV4IpiOo3P8GUoGpz9Pjw0.jpg&w=256&q=75', alt: 'Imagen 2' },
-    { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FzubzDKFoOH4VpfHWvLfhDss6fua.jpg&w=256&q=75', alt: 'Imagen 3' },
-
+    { url: 'https://www.cuevana3.eu/_next/image?url=https%3A%2F%2Fimage.tmdb.org%2Ft%2Fp%2Foriginal%2FzubzDKFoOH4VpfHWvLfhDss6fua.jpg&w=256&q=75', alt: 'Imagen 3' }
   ];
-
 }
