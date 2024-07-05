e61a17a9b48b22b6e4f3f5c644dc8f2b00cbacd3
AleGod125 <alejandroviolori@gmail.com>
2024-07-05 15:02:18 -0500

    diseño canvas

--- src/app/pages/editor/editor.component.ts
+++ src/app/pages/editor/editor.component.ts
@@ -1,20 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-editor',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './editor.component.html',
   styleUrl: './editor.component.css'
 })
 export class EditorComponent {
   colors: string[] = ['blue', 'red', 'green', 'yellow', 'white', 'black']
   Tallas: string[] = ['S', 'M', 'L', 'XL',];
-  ;
+  camisa: string = 'https://static.vecteezy.com/system/resources/previews/008/534/684/original/white-t-shirt-mockup-cutout-file-png.png'
   selectedColor: string = '';
 
   selectColor(color: string): void {
     this.selectedColor = color;
+    alert(color)
   }
 }
