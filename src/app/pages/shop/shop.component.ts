e0b3258809e295540d4274bc602361020036102d
AleGod125 <alejandroviolori@gmail.com>
2024-07-23 15:13:24 -0500

    UI shop

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -2,18 +2,23 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { ShopService } from '../../service/shop.service';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, CommonModule],
+  imports: [RouterOutlet, RouterLink, CommonModule, FormsModule],
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css'
 })
 export class ShopComponent implements OnInit {
 
   private _compraService = inject(ShopService)
-
+  quantities: number[] = Array.from({ length: 10 }, (_, i) => i + 1);
+  selectedQuantity: string | number = 1;
+  editingText = false;
+  editText = '11';
+  mayor:string ="+10";
   listaCompra: string[] = [];
 
 
@@ -23,4 +28,24 @@ export class ShopComponent implements OnInit {
   ElminarTarea(index: number) {
 
   }
+  onQuantityChange(event: Event): void {
+    const selectElement = event.target as HTMLSelectElement;
+    const selectedValue = selectElement.value;
+    if (selectedValue === '0') {
+      console.log('Eliminar producto');
+    } else if (selectedValue === '10+') {
+      this.startEdit()
+    } else {
+      console.log(`Cantidad seleccionada: ${selectedValue}`);
+    }
+  }
+
+  finishEdit(): void {
+    this.editingText = false;
+    this.mayor = this.editText
+  }
+  startEdit(): void {
+    this.editingText = true;
+  }
 }
+
