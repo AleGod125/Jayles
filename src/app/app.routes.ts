5816323c1d2923be5425962adaa85edc1e0ae10f
AleGod125 <alejandroviolori@gmail.com>
2024-08-07 18:53:00 -0500

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { DahsboardComponent } from './pages/dahsboard/dahsboard.component';
 import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 import { EditorComponent } from './pages/editor/editor.component';
 import { ShopComponent } from './pages/shop/shop.component';
+import { CompraComponent } from './pages/compra/compra.component';
 
 export const routes: Routes = [
     {path: '', component:DahsboardComponent},
     {path: 'catalogo', component:CatalogoComponent},
     {path: 'editor', component:EditorComponent},
     {path: 'pago', component:ShopComponent},
+    {path: 'compra', component:CompraComponent},
+
 
 
 
